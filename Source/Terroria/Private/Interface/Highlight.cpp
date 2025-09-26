1314e617b1708714dc61fc04cb2221239c7e35ba
Xerlocked <sherlock203b@gmail.com>
2025-09-26 19:27:29 +0900

    Asset update

---
+++ Source/Terroria/Private/Interface/Highlight.cpp
@@ -0,0 +1,6 @@
+// Copyright 2025 @xerlock. All Rights Reserved.
+
+
+#include "Interface/Highlight.h"
+
+// Add default functionality here for any IHighlight functions that are not pure virtual.
---
+++ Source/Terroria/Public/Interface/Highlight.h
@@ -0,0 +1,26 @@
+// Copyright 2025 @xerlock. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "Highlight.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class UHighlight : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class TERRORIA_API IHighlight
+{
+	GENERATED_BODY()
+
+public:
+	virtual void ActiveHighlightActor() = 0;
+	virtual void DeactiveHighlightActor() = 0;
+};
