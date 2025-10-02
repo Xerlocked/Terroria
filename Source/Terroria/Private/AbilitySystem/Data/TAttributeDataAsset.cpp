8761ddf6fd712d392b3e3e81d4eff8aba5d57816
Xerlocked <sherlock203b@gmail.com>
2025-10-02 16:04:57 +0900

    Attribute Data Asset

---
+++ Source/Terroria/Private/AbilitySystem/Data/TAttributeDataAsset.cpp
@@ -0,0 +1,22 @@
+﻿// Copyright 2025 @xerlock. All Rights Reserved.
+
+
+#include "AbilitySystem/Data/TAttributeDataAsset.h"
+
+FTAttributeData UTAttributeDataAsset::FindAttributeDataByTag(const FGameplayTag& AttributeTag, bool bLogNotFound) const
+{
+	for (const FTAttributeData& AttributeData : AttributeDataContainer)
+	{
+		if (AttributeData.AttributeTag.MatchesTagExact(AttributeTag))
+		{
+			return AttributeData;
+		}
+	}
+
+	if (bLogNotFound)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Attribute data not found: %s"), *AttributeTag.ToString());
+	}
+	
+	return FTAttributeData();
+}
